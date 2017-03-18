907f1e2432e56e6310560b1b9b29fe379d11860c
Jesus Hilario <jesushilariohernandez@gmail.com>
2017-03-18 08:36:38 -0500

    added new folder and files within.

---
+++ Including Files/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    // Including an internal file
+    include("includedfile.php");
+
+    // Including an external file
+    echo file_get_contents("https://www.ecowebhosting.co.uk");
+
+ ?>
